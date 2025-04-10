caf61acf8a65ec20237015cb4102d9eeab29aacc
jojopratama <agnarjonatan@gmail.com>
2025-04-10 11:46:18 +0700

    Penjualan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <!-- /.login-logo -->
             <div class="card card-outline card-primary">
                 <div class="card-header text-center">
-                    <a href="../../index2.html" class="h1"><b>Kasir</b>Laravel</a>
+                    <a href="../../index2.html" class="h1"><b>Kasir</b>Jojo</a>
                 </div>
                 <div class="card-body">
                     <p class="login-box-msg">Sign in to start your session</p>
@@ -51,19 +51,7 @@
                         </div>
                     </form>
 
-                    <div class="social-auth-links text-center mt-2 mb-3">
-                        <a href="#" class="btn btn-block btn-primary">
-                            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                        </a>
-                        <a href="#" class="btn btn-block btn-danger">
-                            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                        </a>
-                    </div>
-                    <!-- /.social-auth-links -->
 
-                    <p class="mb-1">
-                        <a href="forgot-password.html">I forgot my password</a>
-                    </p>
                     <p class="mb-0">
                         <a href="{{ route('register') }}" class="text-center">daftar sebagai petugas</a>
                     </p>
